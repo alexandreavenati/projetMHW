7874920c6f71a243e50a1377d329d9e2dc948c73
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-18 10:33:05 +0100

    Initialisation du projet

---
+++ view/index.php
@@ -0,0 +1,19 @@
+<?php require_once('../config.php') ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Site Monster Hunter Wilds">
+    <title>Monster Hunter Wilds</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
