8e7ff9b2a34a674fcddb3e61dfd08acce14106a7
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-18 16:51:08 +0100

    Section 1 du projet MHW

--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -1,3 +1,5 @@
 <main>
-
+    <section>
+        <h2>La chasse est ouverte</h2>
+<?php require_once('section1.php'); ?>
 </main>
---
+++ view/partial/section1.php
@@ -0,0 +1,10 @@
+<article>
+    <h3>La zone interdite est a portee</h3>
+    <p>
+        Dans Monster Hunter Wilds, les Terres orientales constituent une région riche en ressources rares et en biodiversité. Les chasseurs y explorent des environnements variés, tels que les Plaines venteuses et la Forêt écarlate, à la recherche de matériaux précieux comme le Pollen de fleur de lune, essentiel pour améliorer leur équipement. Ces zones sont également le territoire de monstres redoutables, notamment le Rey Dau, une wyverne volante dominante des Plaines venteuses, et la Lala Barina, une créature arachnide qui rôde dans la Forêt écarlate. Les chasseurs doivent s'adapter aux conditions climatiques dynamiques et aux cycles jour/nuit, qui influencent le comportement des créatures et la disponibilité des ressources. Le village de Kunafa, niché au cœur des Plaines venteuses, sert de base d'opérations aux chasseurs, offrant des services essentiels et des montures Seikrets pour faciliter leurs expéditions.
+    </p>
+</article>
+
+<div><img src="../img/arkveld.png" alt="image de Arkveld"></div>
+
+</section>
