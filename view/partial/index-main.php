a88a2dd2fd77e5af8569c19488562cc904a89d1d
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-19 13:58:59 +0100

    Section 4 du projet MHW

--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -1,16 +1,21 @@
 <main>
-    <section>
+    <section id="chasse">
         <h2>La chasse est ouverte</h2>
         <?php require_once('section1.php'); ?>
     </section>
     
-    <section>
+    <section id="menace">
         <h2>Nouvelles menaces</h2>
         <?php require_once('section2.php'); ?>
     </section>
 
-    <section>
+    <section id="end-game">
         <h2>Contenu End-Game</h2>
         <?php require_once('section3.php'); ?>
+    </section>
+
+    <section id="biome">
+        <h2>Biomes etendus et vivants</h2>
+        <?php require_once('section4.php'); ?>
     </section>
 </main>
---
+++ view/partial/section4.php
@@ -0,0 +1,14 @@
+<div><video src="../video/RayDau.mp4" autoplay muted></video></div>
+
+<article>
+    <h3>Il crache des eclairs, c'est chiant aussi</h3>
+    <div>
+        <p>
+            Son corps est recouvert d'écailles dorées osseuses, avec des écailles noires plus petites sur le ventre, les articulations et les ailes. Contrairement à la plupart des autres wyvernes bipèdes, ses ailes ne sont pas composées de doigts allongés, mais d'une seule membrane semblable à celle des ptérosaures, avec des protubérances tranchantes utilisées comme armes.
+        </p>
+
+        <div><h4 class="animation">Lancer l'animation</h4></div>
+    </div>
+</article>
+
+<div class="animate"><img src="../img/rayDau.png" alt="RayDau"></div>
